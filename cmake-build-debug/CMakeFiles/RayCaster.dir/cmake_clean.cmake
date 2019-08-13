fef9e4ec27f19d3ef3d1b6dd05376a8d0f43e301
Raman Mangla <ramanmangla7@gmail.com>
2019-08-13 01:16:23 -0400

    Added basic SFML dependencies and CMake files

---
+++ cmake-build-debug/CMakeFiles/RayCaster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayCaster.dir/main.cpp.o"
+  "RayCaster.pdb"
+  "RayCaster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayCaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
